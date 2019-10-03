e177ff95fe5e31064fd900abfabb3acff26e34fc
Devin French <dmf119@gmail.com>
2019-10-03 01:00:22 -0700

    Set windows desktop background to image

--- wallpaper.py
+++ wallpaper.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import ctypes
+import os
 
 url = "https://www.reddit.com/r/earthporn/.json"
 agent = "EarthPorn Wallpaper"
@@ -7,5 +9,9 @@ r = requests.get(url, headers={"User-agent": agent})
 data = json.loads(r.content)
 img_url = data["data"]["children"][0]["data"]["url"]
 img_data = requests.get(img_url).content
-with open("wallpaper" + img_url[-4:], "wb") as file:
+file_name = "wallpaper" + img_url[-4:]
+with open(file_name, "wb") as file:
     file.write(img_data)
+
+img_path = os.path.join(os.getcwd(), file_name)
+ctypes.windll.user32.SystemParametersInfoW(20, 0, img_path, 0)
