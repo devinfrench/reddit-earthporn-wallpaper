9bcef61bd33e7375cbae499d3414e2363243847e
Rusty Balboa <rustybalboadev@gmail.com>
2020-10-05 11:33:42 -0500

    New Changes

--- wallpaper.py
+++ wallpaper.py
@@ -3,14 +3,30 @@ import json
 import ctypes
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('subreddit', nargs="?", default="wallpapers", help="Subreddit Name To Find Image From Default Subreddit is r/wallpapers")
+p = parser.add_mutually_exclusive_group()
+p.add_argument('--hot', action='store_true', help="Get Hot Posts")
+p.add_argument('--new', action='store_true', help="Get New Posts")
+p.add_argument('--top', action='store_true', help="Get Top Posts")
+p.add_argument('--rising', action='store_true', help="Get Rising Posts")
+args = parser.parse_args()
 
 filetypes = ["jpg", "png"]
-subreddit = "wallpapers"
 
-if len(sys.argv) > 1:
-    subreddit = sys.argv[1]
+subreddit = args.subreddit
 
-url = "https://www.reddit.com/r/" + subreddit + "/.json"
+if args.hot:
+    url = "https://www.reddit.com/r/" + subreddit + "/hot/.json"
+elif args.new:
+    url = "https://www.reddit.com/r/" + subreddit + "/new/.json"
+elif args.top:
+    url = "https://www.reddit.com/r/" + subreddit + "/top/.json"
+elif args.rising:
+    url = "https://www.reddit.com/r/" + subreddit + "/rising/.json"
+
 agent = "Reddit Wallpaper"
 r = requests.get(url, headers={"User-agent": agent})
 data = json.loads(r.content)
@@ -20,6 +36,7 @@ for child in data["data"]["children"]:
     url = child["data"]["url"]
     if url[-3:] in filetypes:
         img_url = url
+        print(img_url)
         break
 
 img_data = requests.get(img_url).content
